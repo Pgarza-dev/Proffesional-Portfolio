eaa7f4626a073d5cefaa36dba6b9a176f152e121
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-06-04 11:09:17 +0100

    remove pgarza-dev on click

--- js/index.js
+++ js/index.js
@@ -3,11 +3,25 @@ console.log("index.js connected");
 // Hamburguer Menu
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-li');
+const pgarzaDev = document.querySelector('.pgarza-dev');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  if (pgarzaDev.style.display === 'none') {
+    pgarzaDev.style.display = 'block';
+  } else {
+    pgarzaDev.style.display = 'none';
+  }
 });
+
+// hamburger.addEventListener('click', function() {
+//   pgarzaDev.style.display = 'none';
+// });
+// document.getElementsByClassName(".pgarza-dev").style.display = "block";
+
+
+
 
 document.querySelectorAll('.nav-link').forEach((n) =>
   n.addEventListener('click', () => {
